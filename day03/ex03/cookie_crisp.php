bd12f2b507a9f87470e1f289bc43cec3f4b20b89
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-11 14:35:17 +0200

    ex04 done , ex05 pending day 03

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,12 +1,13 @@
+#!/usr/bin/php
 <?php
-    if ($_GET['action'] == 'set')
+    if ($_GET["action"] == "set")
     {
         setcookie($_GET["name"], $_GET["value"], time() + 3600, "/");
         echo "Cookie has been set\n";
     }
     if ($_GET["action"] == "get")
     {
-        echo $_COOKIE[$_GET["name"]];
+        echo "Cookie Name: ".$_COOKIE[$_GET["name"]];
         if  ((strlen($_COOKIE[$_GET["name"]]) > 0))
             echo "\r\n";
         else
@@ -15,6 +16,6 @@
     if ($_GET["action"] == "delete")
     {
         setcookie($_GET["name"], $_GET["value"], time() + 3600, "/");
-        echo ("Cookie is deleted\n")
-;    }
+        echo ("Cookie is deleted\n");
+    }
 ?>
