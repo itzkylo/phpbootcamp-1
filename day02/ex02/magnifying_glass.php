7ef8380c218dc5ae74d14960959a75f88a55696c
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-11 07:56:44 +0200

    it works!

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -1,43 +1,20 @@
 #!/usr/bin/php
 <?php
-    function uprep($in)
-    {
-        return ($in[1].strtoupper($in[2]).$in[3]);
-    }
-
-    function checknchange($in)
-    {
-        $flag = true;
-        $c = 0;
-        while ($in[$c])
-        {
-            if ($in[$c] == ">")
-                $flag = true;
-            if ($in[$c] == "<")
-                $flag = false;
-            if ($flag)
-                $out = ucfirst($in[$c]);
-            else
-                $out .= ucfirst($in[$i]);
-            $c++;
-        }
-        return ($out);
-    }
-
-    function rep_noup($in)
-    {
-        return ($in[1].checknchange($in[2].$in[3]));
-    }
-
-    if ($argc != 2)
-        exit;
-    if (!is_readable($input = $argv[1]))
-    {
-        die("Incorrect file\n");
-    }
-    $data = file_get_contents($input);
-    $data = preg_replace_callback('/(<.*title=\")(.*)(\".*>)/Uis', uprep, $data);
-    $data = preg_replace_callback('/(<a.*>)(.*)(<\/a>)/Uis', rep_noup, $data);
-    echo $data;
-    
+	if ($argc < 2 || !file_exists($argv[1]))
+		exit();
+	$read = fopen($argv[1], 'r');
+	$page = "";
+	while ($read && !feof($read)) {
+		$page .= fgets($read);
+	}
+	$page = preg_replace_callback("/(<a )(.*?)(>)(.*)(<\/a>)/si", function($matches) {
+		$matches[0] = preg_replace_callback("/( title=\")(.*?)(\")/mi", function($matches) {
+			return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
+		}, $matches[0]);
+		$matches[0] = preg_replace_callback("/(>)(.*?)(<)/si", function($matches) {
+			return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
+		}, $matches[0]);
+		return ($matches[0]);
+	}, $page);
+	echo $page;
 ?>
---
+++ day02/ex03/who.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+    date_default_timezone_set("Africa/Johannesburg");
+    echo get_current_user();
+    echo "   ";
+    echo date("M d");
+    echo " ";
+    echo date("h:i");
+?>
